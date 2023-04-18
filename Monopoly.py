06de557ffc5862b16cf92b6c255bba37ecbf1fe2
Sasha <sasalubinskij47@gmail.com>
2023-04-18 19:10:59 +0300

    add class Field

--- Monopoly.py
+++ Monopoly.py
@@ -7,3 +7,13 @@ class Player:
         self.turn = turn
         self.town_list = []
         self.balance = 500
+
+
+class Field:
+    def __init__(self):
+        self.field = ['STR', 'POL', 'LUC', '*?*', 'KYV', 'VIN', '*#*', 'RIV', 'TER', '*$*']
+        self.a_player = ['*A*', '''   ''', '''   ''', '''   ''', '''   ''', '''   ''', '''   ''', '''   ''', '''   ''',
+                         '''   ''']
+        self.b_player = ['*B*', '''   ''', '''   ''', '''   ''', '''   ''', '''   ''', '''   ''', '''   ''', '''   ''',
+                         '''   ''']
+        self.building = [f'''   ''', '(0)', '(0)', '''   ''', '(0)', '(0)', '''   ''', '(0)', '(0)', '''   ''']
